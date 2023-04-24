2985a35272345f79e6384b9d8e5694deaad3c120
Sameep Rana <sameep.monsoon@gmail.com>
2023-04-25 01:32:50 +0545

    animation fixed

--- src/Page Component/Hero/Hero.tsx
+++ src/Page Component/Hero/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = (props: {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.2 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 0.6 }}
           variants={{
             hidden: {
               opacity: 0,
@@ -81,7 +81,7 @@ const Hero = (props: {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.4, duration: 0.5 }}
+          transition={{ delay: 0.3, duration: 0.5 }}
           variants={{
             hidden: {
               opacity: 0,
@@ -128,7 +128,7 @@ const Hero = (props: {
         transition={{ duration: 0.5 }}
         variants={{
           hidden: {
-            opacity: 1,
+            opacity: 0,
             x: 0,
           },
           visible: {
